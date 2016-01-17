60e26ea2a55525fcd23245deb1a75d6e09560d04
Pebbelzz <greeneyes14j@gmail.com>
2016-01-17 11:48:15 -0500

    Submit button with glow

---
+++ Buttons/main.js
@@ -0,0 +1,8 @@
+function button1MouseDown(){
+	document.getElementById("firstButton").style.boxShadow = "inset 0px 0px 8px #666666";
+}
+
+function button1MouseUp(){
+	document.getElementById("firstButton").style.boxShadow = "0px 0px 8px #2eb82e";
+	document.getElementById("firstButton").style.border = "3px solid #2eb82e"
+}
